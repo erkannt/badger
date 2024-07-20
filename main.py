c780079b994e8551d4c2a0a9f43f910f783e8eef
Daniel Haarhoff <daniel@rknt.de>
2024-07-20 12:38:15 +0100

    Introduce concept of programs

--- main.py
+++ main.py
@@ -3,17 +3,61 @@ from pngdec import PNG
 
 badger = badger2040.Badger2040()
 png = PNG(badger.display)
- 
-png.open_file("daniel.png")
-png.decode(0,0)
 
-# badger.set_pen(15)
-# badger.clear()
-# badger.set_pen(0)
-# badger.set_thickness(7)
-# badger.set_font("serif")
-# badger.text("daniel", 20, 80, scale=2.5)
-# badger.set_font("sans")
-# badger.set_thickness(2)
-# badger.text("he/him", 20, 20, scale=0.8)
-badger.update()
+prev_program = 1
+program = 1
+program_count = 2
+
+
+def white():
+    badger.set_pen(15)
+    badger.clear()
+
+
+def update(speed=2):
+    badger.set_update_speed(speed)
+    badger.update()
+
+
+def daniel_png():
+    if prev_program == program:
+        return
+    white()
+    png.open_file("daniel.png")
+    png.decode(0, 0)
+    update()
+
+
+def daniel_txt():
+    if prev_program == program:
+        return
+    white()
+    badger.set_pen(0)
+    badger.set_thickness(7)
+    badger.set_font("serif")
+    badger.text("daniel", 20, 80, scale=2.5)
+    badger.set_font("sans")
+    badger.set_thickness(2)
+    badger.text("he/him", 20, 20, scale=0.8)
+    update()
+
+
+programs = [
+    daniel_png,
+    daniel_txt,
+]
+
+
+def clamp(n, smallest, largest):
+    return max(smallest, min(n, largest))
+
+
+while True:
+    prev_program = program
+    if badger.pressed(badger2040.BUTTON_UP):
+        program = clamp(program + 1, 1, program_count)
+    if badger.pressed(badger2040.BUTTON_DOWN):
+        program = clamp(program - 1, 1, program_count)
+    if prev_program != program:
+        print(program)
+    programs[program - 1]()
